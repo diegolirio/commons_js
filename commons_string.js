5e5e5bca478ab17d9d38c31f221cfd3c022e0ef9
Diego Lirio <diegolirio.dl@gmail.com>
2014-06-04 15:50:11 -0300

    Update commons_string.js
    padLeft

--- commons_string.js
+++ commons_string.js
@@ -4,4 +4,13 @@
       alert('test_commons_string');
   }
 
+	//examples
+	//console.log(padLeft(23,5));       //=> '00023'
+	//console.log((23).padLeft(5));     //=> '00023'
+	//console.log((23).padLeft(5,' ')); //=> '   23'
+	//console.log(padLeft(23,5,'$'));  //=> '$$$$$$23'	 
+	function padLeft(value, length, valueAdd) {
+		return Array(length-String(value).length+1).join(valueAdd||'0')+value;
+	}	
+
 </script>
