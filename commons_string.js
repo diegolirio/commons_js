53bf0d20bab3950964de798c2b8ae824af573bc2
Diego Lirio <diegolirio.dl@gmail.com>
2014-06-04 15:52:53 -0300

    Update commons_string.js
    padding_left / padding_right

--- commons_string.js
+++ commons_string.js
@@ -13,4 +13,36 @@
 		return Array(length-String(value).length+1).join(valueAdd||'0')+value;
 	}	
 
+
+	// left padding s with c to a total of n chars
+	//padding_left('abcd', '0', 8);                        // 0000abcd
+	function padding_left(s, c, n) {
+	    if (! s || ! c || s.length >= n) {
+	        return s;
+	    }
+	
+	    var max = (n - s.length)/c.length;
+	    for (var i = 0; i < max; i++) {
+	        s = c + s;
+	    }
+	
+	    return s;
+	}
+	
+	// right padding s with c to a total of n chars
+	//padding_right('1234', '0', 9);                       // 123400000	
+	function padding_right(s, c, n) {
+	    if (! s || ! c || s.length >= n) {
+	        return s;
+	    }
+	
+	    var max = (n - s.length)/c.length;
+	    for (var i = 0; i < max; i++) {
+	        s += c;
+	    }
+	
+	    return s;
+	}
+
+
 </script>
